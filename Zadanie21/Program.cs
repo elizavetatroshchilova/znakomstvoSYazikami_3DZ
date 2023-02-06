cb574c3649fe6be1f8e3545972f48a6a1eb0887e
Elizabeth <qazwsx050816@gmail.com>
2023-02-06 19:13:15 +0300

    Add 3 homework

---
+++ Zadanie21/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 21
+
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+// A (3,6,8); B (2,1,-7), -> 15.84
+
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+
+Console.WriteLine("ВВедите координаты х1, у1, z1 для первой точки");
+int x1 = int.Parse(Console.ReadLine());
+int y1 = int.Parse(Console.ReadLine());
+int z1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("ВВедите координаты х2, у2, z2 для второй точки");
+int x2 = int.Parse(Console.ReadLine());
+int y2 = int.Parse(Console.ReadLine());
+int z2 = int.Parse(Console.ReadLine());
+
+int result = (int)Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1),2) + Math.Pow((z2 - z1), 2));
+Console.WriteLine($"Расстояние между введенными точками равно {result}");
---
+++ Zadanie23/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.WriteLine("Введите число");
+int.TryParse(Console.ReadLine(), out int n); 
+int index = 1;
+while (index <= n)
+{
+   int result = (int)Math.Pow(index, 3); 
+   Console.Write($"{result} ");
+   index++;
+}
